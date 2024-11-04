99766f310d35a755b666663a31903cd047d7734d
Nick VIntartas <nv2009123@gmail.com>
2024-11-04 11:11:41 -0600

    Initial Commit

---
+++ CYOAVintartasTests/CYOAVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  CYOAVintartasTests.swift
+//  CYOAVintartasTests
+//
+//  Created by NICHOLAS VINTARTAS on 11/4/24.
+//
+
+import Testing
+@testable import CYOAVintartas
+
+struct CYOAVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
